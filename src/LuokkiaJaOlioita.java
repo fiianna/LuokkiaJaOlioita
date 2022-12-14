2e833f573baf586425a93f7bba919d2c77ce72f8
Fiia <fiianna.h@gmail.com>
2022-12-14 16:45:59 +0200

    Initial commit

---
+++ src/LuokkiaJaOlioita.java
@@ -0,0 +1,60 @@
+
+
+public class LuokkiaJaOlioita {
+
+	public static void main(String[] args) {
+		
+		// oliot
+		Auto auto1 = new Auto("Lexus", "NX 450h", "hybrid");
+		
+		
+		// Tulosta tiedot -metodi
+		auto1.tulostaTiedot();
+		//auto2.tulostaTiedot();
+		
+		// Muuta tiedot -metodi
+		
+		
+		// Olioiden arvot
+		// auto.merkki = "Lexus";
+		
+		// Kutsutaan metodia
+		auto1.kaynnisty();
+		//System.out.println(auto.merkki);
+	}
+
+}
+
+class Auto{
+	// Ominaisuudet
+	public String merkki;
+	public String malli;
+	public String polttoaine;
+	
+	// Muodostin/parametrisoitu konstruktori luokasta Auto
+	public Auto(String merkki, String malli, String polttoaine) {
+		this.merkki=merkki;
+		this.malli=malli;
+		this.polttoaine=polttoaine;
+	}
+	
+	// Toiminnallisuudet/metodit
+	public void kaynnisty() {
+		System.out.println("Ajoneuvo on käynnistetty.");
+	}
+	
+	public void ajaAutoa(){
+		System.out.println("Ajat autoa.");
+	}
+	
+	public void sammuta() {
+		System.out.println("Ajoneuvo on sammutettu.");
+	}
+	
+	public void tulostaTiedot() {
+		System.out.println("AUTO:");
+		System.out.println(merkki);
+		System.out.println(malli);
+		System.out.println(polttoaine);
+	}
+}
